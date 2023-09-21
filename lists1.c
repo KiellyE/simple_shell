14817b3a022d92d0673a5c571d6e11d1f7cd8597
aileswhale <ucheodilach@gmail.com>
2023-09-21 01:16:43 +0100

    modified

--- lists1.c
+++ lists1.c
@@ -9,7 +9,7 @@
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h)
 	{
 		h = h->next;
@@ -30,7 +30,7 @@ char **list_to_strings(list_t *head)
 	size_t i = list_len(head), j;
 	char **strs;
 	char *str;
-	
+
 	if (!head || !i)
 		return (NULL);
 	strs = malloc(sizeof(char *) * (i + 1));
@@ -88,7 +88,7 @@ size_t print_list(const list_t *h)
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
 	char *p = NULL;
-	
+
 	while (node)
 	{
 		p = starts_with(node->str, prefix);
@@ -110,7 +110,7 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 ssize_t get_node_index(list_t *head, list_t *node)
 {
 	size_t i = 0;
-	
+
 	while (head)
 	{
 		if (head == node)
